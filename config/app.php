c2bc27717606377489a37733073cd4dd79eb1f34
michal3085 <michal3085@gmail.com>
2023-01-15 18:16:52 +0100

    https try 3

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://karolinaimaciek.pl/under/construction'),
+    'url' => env('APP_URL', 'https://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
