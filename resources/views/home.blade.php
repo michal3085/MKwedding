39b33011de8430e05ebc2f6f328764b402fa86c5
michal3085 <michal3085@gmail.com>
2023-01-28 17:51:04 +0100

    Czadowa Fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -218,7 +218,7 @@
 {{--      background-size: cover;
   background-repeat: no-repeat;
   position: relative;--}}
-<div id="fh5co-gallery" class="fh5co-section-gray" style="background-image: url('/images/czad.png'); background-size: cover; background-repeat: no-repeat;">
+<div id="fh5co-gallery" class="fh5co-section-gray" style="background-image: url('/images/czad.png'); background-size: 130%; background-repeat: no-repeat;">
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
@@ -228,14 +228,15 @@
         </div>
         <div class="row row-bottom-padded-md">
             <div class="col-md-12">
-                <ul id="fh5co-gallery-list">
+{{--                <ul id="fh5co-gallery-list">--}}
 {{--                    <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url('{{ asset('/images/czadowalogo.jpg') }}'); ">--}}
 {{--                    </li>--}}
 {{--                        <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url('{{ asset('/images/czad.jpg') }}'); ">--}}
 {{--                        </li>--}}
-                    <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url('{{ asset('/images/czadowa_para.jpg') }}'); width: 50%;">
-                    </li>
-                </ul>
+{{--                    <li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url('{{ asset('/images/czadowa_para.jpg') }}'); width: 50%;">--}}
+{{--                    </li>--}}
+{{--                </ul>--}}
+                <img class="img-czadowa" src="{{ asset('/images/czadowa_para.jpg') }}">
             </div>
         </div>
     </div>
