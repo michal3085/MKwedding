e05dc84fbc15f71f14e7fec06b5ecb884b742155
michal3085 <michal3085@gmail.com>
2023-02-03 20:08:38 +0100

    Fixed add age migration problem

--- database/migrations/2023_02_03_190625_add_age_to_guest.php
+++ database/migrations/2023_02_03_190625_add_age_to_guest.php
@@ -13,7 +13,7 @@ class AddAgeToGuest extends Migration
      */
     public function up()
     {
-        Schema::table('guest', function (Blueprint $table) {
+        Schema::table('guests', function (Blueprint $table) {
             $table->integer('age')->after('child')->nullable();
         });
     }
@@ -25,7 +25,7 @@ class AddAgeToGuest extends Migration
      */
     public function down()
     {
-        Schema::table('guest', function (Blueprint $table) {
+        Schema::table('guests', function (Blueprint $table) {
             $table->dropColumn('age');
         });
     }
