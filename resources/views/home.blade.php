744b2613bd4e3953702a38545a7aba556fa85ac3
michal3085 <michal3085@gmail.com>
2023-02-26 20:13:32 +0100

    main page desc fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -78,7 +78,10 @@
             <div class="row animate-box">
                 <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
                     <h2 style="color: #F14E95;">Będziesz?</h2>
-                    <p style="color: white">Wypełnij oba pola, aby potwierdzić swoją obecność. Więcej informacji o zakwaterowaniu i transporcie znajdziesz poniżej.</p>
+                    <p style="color: white">Wypełnij oba pola, aby potwierdzić swoją obecność. Więcej informacji o zakwaterowaniu i transporcie znajdziesz poniżej.
+                    <br/>
+                        (Proszę potwierdzać osoby pojedynczo)
+                    </p>
                 </div>
             </div>
             <div class="row animate-box">
